403e5c315315070a94a2a20fa800f991e6c8ce16
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-05-05 15:54:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Nested_Arrays_Notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Carla Baysinger
+            // Nested Arrays Notes
+            // May 5th 2024
+
+            //what is a nested if?
+
+        }//Main
+    }//class
+}//namespace
